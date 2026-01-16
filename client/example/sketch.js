0bc454f37087d38e4152d0cc06613313b478f65a
littlebrother12321 <eldredgething4@gmail.com>
2026-01-15 20:06:46 -0700

    fixed issues with camera (added another pop();)

--- client/example/sketch.js
+++ client/example/sketch.js
@@ -8,6 +8,23 @@ let sprite_sheet;
 let camera;
 // The gravity
 const g = 0.1;
+// The x-equivalent of gravity
+const FRICTION = 0.5
+
+
+// Visual stuff
+const halfScreenX = window.innerWidth/2;
+const halfScreenY = window.innerHeight/2;
+let floorY = -16;
+
+//The player Pos using X and Y
+let x = 0;
+let y = floorY;
+
+let vx = 0;
+let vy = 0;
+
+let sentInitialState = false;
 
 
 // WebSocket stuff
@@ -29,12 +46,18 @@ function setup() {
 
     drawingContext.disable(drawingContext.DEPTH_TEST);
 
+    // connect to websocket and set update interval
     socket = new WebSocket("ws://localhost:8080");
-
+    setInterval(sendState, 50); // 20 Hz
+    
+    // do stuff when socket is opened
     socket.onopen = () => {
 	console.log("socket connected");
+	sendState();
+	sendInitialState = true;
     };
 
+    // do stuff when message is recieved
     socket.onmessage = (event) => {
 	const msg = JSON.parse(event.data);
 
@@ -78,79 +101,52 @@ function setup() {
     fill(0,200,0);
    //myColor = color(random(255), 128, random(255));
 }
+
+// close socket before refreshing page
+window.addEventListener("beforeunload", () => {
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    socket.close();
+  }
+});
+
 // we gonna do stoof.
-
-const halfScreenX = window.innerWidth/2;
-const halfScreenY = window.innerHeight/2;
-//The player Pos using X and Y
-let x = 0;
-let y = 0;
-let floor = 0;
-
-let vx = 0;
-let vy = 0;
-
-let sentInitialState = false;
 
 // All the list of the players (now at the top of file
 //The draw function draws things
 function draw() {
 
+    //drawing the floor
     push();
     clear();
     //rect(0,floor,window.innerWidth,halfScreenY);
     push();
-    translate(0, halfScreenY + 16, 0);
+    translate(0, halfScreenY, 0);
     plane(window.innerWidth, window.innerHeight);
     pop();
-  
-    // cool gravity equations (Yeah.)
 
-    if (y < floor) {
-	vy += g;
-    } else {
-	y = floor;
-	vy = 0;
-    }
-    //if (y >= floor) {
-	//vy = 0;
-	//y = floor;
-    //}
-    // Friction equations
-    if (vx > 0){
-	vx -= .5;
-    } else if (vx < 0){
-	vx += .5;
-    }
-    x += vx;
-    y += vy;
+    // Camera codes
+    camera.setPosition(x, y - 100, 500);
+    camera.lookAt(x, y, 0);
+    //orbitControl();
+
+    // Grid for reference
+    drawFloorGrid();
+
+    // Runs updatephysics, look below for details
+    updatePhysics();
+    
     // animation(sprite_sheet, x, y); This line got comented out
     //console.log("x: " + x + " y: " + y + " vx: " + vx + " vy: " + vy);
     //console.log("camX: " + camera.eyeX, "camY: " + camera.eyeY);
-    // moving around code
-    if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && ( y >= floor)) vy -= 2;
-    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) y += 1;
-    if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && (Math.abs(vx) <= 5)) vx -= 1;
-    if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && (Math.abs(vx) <= 5)) vx += 1;
-     //Sprinting
-    if (keyIsDown(16)) { // shift speed boost
-        if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && (y >= floor)) vy -= 2;
-        if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) y += 1;
-        if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && (Math.abs(vx) <= 7)) vx -= 1;
-        if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && (Math.abs(vx) <= 7)) vx += 1;
-    };
 
-    camera.setPosition(x, y, -300);
-    camera.lookAt(x, y, 0);
-    //orbitControl();
-        //loop though players and draws them
+    //loop though players and draws them
     for (let id in players) {
 	if (id !== myId) {
             push();
             //  fill(players[id].color);
 	    translate(
-		players[id].x - x,
-		players[id].y - y,
+		players[id].x,
+		players[id].y,
 		1
 	    );
 	    animation(sprite_sheet, 0, 0);
@@ -159,16 +155,69 @@ function draw() {
     }
     // play animation at location of camera x y.
     push();
-    //translate(x, y, 0);
+    translate(x, y, 0);
     animation(sprite_sheet, 0, 0);
     pop();
-// send over your player data to server
-    if (!sentInitialState == false || vx !== 0 && vy !== 0) {
-	sendState();
-	sentInitialState = true;
-    }
+
+    
+    // send over your player data to server
+    sendState();
+    
+    // Handle socket errors and closes
+    socket.onerror = (err) => {
+	console.error("socket error", err);
+    };
+
+    socket.onclose = () => {
+	console.log("socket closed");
+    };
+    pop();
 } // end of draw function
 
+function updatePhysics() {
+    // cool gravity equations (Yeah.)
+    vy += g;
+    y += vy;
+
+    // Hit floor
+    if (y > floorY) {
+	y = floorY;
+	vy = 0;
+    }
+    
+    // Friction equations
+    if (vx > 0)	vx = max(0, vx - FRICTION);
+    else if (vx < 0) vx = min(0, vx + FRICTION);
+    x += vx;
+
+    // moving around code
+    if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && y === floorY) vy = -2;
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) y += 1;
+    if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && (Math.abs(vx) <= 5)) vx -= 1;
+    if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && (Math.abs(vx) <= 5)) vx += 1;
+     //Sprinting
+    if (keyIsDown(16)) { // shift speed boost
+        if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && y === floorY) vy = -4;
+        if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) y += 1;
+        if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && (Math.abs(vx) <= 7)) vx -= 1;
+        if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && (Math.abs(vx) <= 7)) vx += 1;
+    };
+}
+
+function drawFloorGrid() {
+    push();
+    translate(0, floorY + 16, 0); //move to floor
+    stroke(0, 255, 0);
+    strokeWeight(1);
+
+    const size = 50; // Spaces between lines
+    const extent = 2000; // How far lines go
+    for(let i = -extent; i <= extent; i += size) {
+	line(i, 0, -extent, i, 0, extent); // Vertical lines
+	line(-extent, 0, i, extent, 0, i); // Horizontal lines
+    }
+    pop();
+}
 
 function sendState() {
   if (!socket || socket.readyState !== WebSocket.OPEN) return;
@@ -177,6 +226,8 @@ function sendState() {
     x, y, vx, vy
   }));
 }
+
+
 
 
 //  // onConnection, when connected to server, server wants your uid
